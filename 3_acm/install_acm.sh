d727654a9da9ab29f5c6d28b23bae9d43492905c
Vic Iglesias <viglesias@google.com>
2019-10-10 13:18:52 -0700

    Use correct key locations for ACM install

--- 3_acm/install_acm.sh
+++ 3_acm/install_acm.sh
@@ -1,37 +1,31 @@
-#!/bin/bash
+#!/bin/bash -xe
+
+if [ -z ${GITLAB_HOSTNAME} ];then
+  read -p "What is the GitLab hostname (i.e. my.gitlab.server)? " GITLAB_HOSTNAME
+fi
 
 gcloud container clusters get-credentials anthos-platform-prod-central --region us-central1
+kubectl config delete-context prod-central
 kubectl config rename-context $(kubectl config current-context) prod-central
 
 gcloud container clusters get-credentials anthos-platform-prod-east --region us-east1
+kubectl config delete-context prod-east
 kubectl config rename-context $(kubectl config current-context) prod-east
 
 gcloud container clusters get-credentials anthos-platform-staging --region us-central1
+kubectl config delete-context staging
 kubectl config rename-context $(kubectl config current-context) staging
 
-gcloud container clusters get-credentials anthos-platform-ci --region us-central1
-kubectl config rename-context $(kubectl config current-context) ci
-
-gcloud container clusters get-credentials gitlab --region us-central1
-kubectl config rename-context $(kubectl config current-context) gitlab
-
-
-for CONTEXT in prod-central prod-east staging ci; do
+for CONTEXT in prod-central prod-east staging; do
   kubectl config use-context ${CONTEXT}
   kubectl apply -f config-management-operator.yaml
-  KEYNAME=${CONTEXT}-ssh
-  mkdir -p ssh-keys
-  ssh-keygen -t rsa -b 4096 -C "[${CONTEXT}]" \
-     -N '' -f ssh-keys/${KEYNAME}
+  KEYNAME=${CONTEXT}
   kubectl delete secret git-creds --namespace=config-management-system || true
   kubectl create secret generic git-creds --namespace=config-management-system \
-          --from-file=ssh=ssh-keys/${KEYNAME}
+          --from-file=ssh=../ssh-keys/${KEYNAME}
   GITLAB_ADDRESS=$(gcloud compute addresses describe gitlab --region us-central1 --format 'value(address)')
-  export GITLAB_HOSTNAME=gitlab.${GITLAB_ADDRESS}.xip.io
+  export GITLAB_HOSTNAME=${GITLAB_HOSTNAME}
   export CONTEXT=${CONTEXT}
   cat config-management.yaml.tpl | envsubst > config-management-${CONTEXT}.yaml
   kubectl apply -f config-management-${CONTEXT}.yaml
 done
-
-GITLAB_PASSWORD=$(kubectl get secrets gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d)
-echo "Gitlab root password is: ${GITLAB_PASSWORD}"
