4cce678e1351e68b59d7812325cb94516f5560e5
Henry Bell <henrybell@google.com>
2020-03-27 16:54:35 +0000

    Modularise cluster build to include Artifact Registry permissions
    Change-Id: Icd28511d9552bc7eba38d6186fc6e2e8387ab84e

--- 1_clusters/outputs.tf
+++ 1_clusters/outputs.tf
@@ -1,3 +1,7 @@
+output "development__cluster-service-account" {
+  value       = "${module.anthos-platform-dev.service_account}"
+  description = "Service account used to create the cluster and node pool(s)"
+}
 
 output "staging__cluster-service-account" {
   value       = "${module.anthos-platform-staging.service_account}"
@@ -11,10 +15,5 @@ output "prod-east__cluster-service-account" {
 
 output "prod-central__cluster-service-account" {
   value       = "${module.anthos-platform-prod-central.service_account}"
-  description = "Service account used to create the cluster and node pool(s)"
-}
-
-output "development__cluster-service-account" {
-  value       = "${module.anthos-platform-dev.service_account}"
   description = "Service account used to create the cluster and node pool(s)"
 }
