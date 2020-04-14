90392a238e02f94bd5aaa5b2d755e3dd8d274ef7
Ben Good <benjamingood@google.com>
2020-04-14 11:28:46 -0600

    Adding clean up before test and only clean-up after test if successful. Also cleaning up AR and hipster/petabank SAs
    Change-Id: I3af976ea247c7a45344ed6a1f86f310160580b38

--- tests/setup_teardown.sh
+++ tests/setup_teardown.sh
@@ -8,15 +8,22 @@ project_id=anthos-platform-ci-env
 domain=ap-anthos-platform-ci-env.cloud-tutorial.dev
 
 # Activate the service account
-gcloud auth activate-service-account --key-file="${KOKORO_KEYSTORE_DIR}/${sa_creds}"
+#gcloud auth activate-service-account --key-file="${KOKORO_KEYSTORE_DIR}/${sa_creds}"
 
 # Display commands, now that creds are set.
 set -x
 
-gcloud config set project ${project_id}
+#gcloud config set project ${project_id}
 
+# Make sure the project is clean before running the setup
+gcloud builds submit --config=cloudbuild-destroy.yaml
 gcloud builds submit --config=cloudbuild.yaml --substitutions=_DOMAIN=${domain}
-sleep 5m
-gcloud builds submit --config=cloudbuild-gcloud-destroy.yaml
+
+# If the setup succeeded then tear it down.
+if [ $? -eq 0 ]; then
+	sleep 5m
+	# Clean up after the run
+	gcloud builds submit --config=cloudbuild-destroy.yaml
+fi
 
 echo "All passed"
