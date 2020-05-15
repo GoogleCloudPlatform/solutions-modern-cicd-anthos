00c45967439ddac8a83a14f234e467bb77cef9bc
Morgante Pell <morgantep@google.com>
2020-05-14 21:17:33 -0400

    Switch services to use project-services module

--- 0_foundation/services.tf
+++ 0_foundation/services.tf
@@ -14,38 +14,17 @@
  * limitations under the License.
  */
 
-resource "google_project_service" "compute" {
-  project = var.project_id
-  service = "compute.googleapis.com"
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 8.0"
 
-  disable_dependent_services = true
-  depends_on                 = [google_project_service.cloudresourcemanager]
-}
+  project_id = var.project_id
 
-resource "google_project_service" "container" {
-  project = var.project_id
-  service = "container.googleapis.com"
-
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "cloudresourcemanager" {
-  project = var.project_id
-  service = "cloudresourcemanager.googleapis.com"
-
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "sqladmin" {
-  project = var.project_id
-  service = "sqladmin.googleapis.com"
-
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "artifactregistry" {
-  project = var.project_id
-  service = "artifactregistry.googleapis.com"
-
-  disable_dependent_services = true
+  activate_apis = [
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "sqladmin.googleapis.com",
+    "artifactregistry.googleapis.com"
+  ]
 }
