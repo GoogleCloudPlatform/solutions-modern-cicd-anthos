810c8cf3dc9f5917890fdf1f760204edf75105ed
Morgante Pell <morgantep@google.com>
2020-05-14 22:05:25 -0400

    Upgrade GitLab to Terraform 0.12 style

--- 2_gitlab/gitlab-repos/repos.tf
+++ 2_gitlab/gitlab-repos/repos.tf
@@ -15,9 +15,9 @@
  */
 
 provider "gitlab" {
-    token = "${var.gitlab_token}"
-    base_url = "https://${var.gitlab_hostname}/api/v4/"
-    insecure = true
+  token    = var.gitlab_token
+  base_url = "https://${var.gitlab_hostname}/api/v4/"
+  insecure = true
 }
 
 locals {
@@ -25,172 +25,173 @@ locals {
 }
 
 resource "gitlab_group" "platform-admins" {
-  name        = "platform-admins"
-  path        = "platform-admins"
-  description = "An group of projects for Platform Admins"
+  name             = "platform-admins"
+  path             = "platform-admins"
+  description      = "An group of projects for Platform Admins"
   visibility_level = "internal"
 }
 
 resource "gitlab_project" "anthos-config-management" {
-  name         = "anthos-config-management"
-  description  = "Anthos Config Management repo"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "anthos-config-management"
+  description      = "Anthos Config Management repo"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "shared-kustomize-bases" {
-  name         = "shared-kustomize-bases"
-  description  = "Kubernetes Application Configuration Bases"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "shared-kustomize-bases"
+  description      = "Kubernetes Application Configuration Bases"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "kaniko-docker" {
-  name         = "kaniko-docker"
-  description  = "Docker+Kaniko Docker image"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "kaniko-docker"
+  description      = "Docker+Kaniko Docker image"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "kustomize-docker" {
-  name         = "kustomize-docker"
-  description  = "Kustomize Docker image"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "kustomize-docker"
+  description      = "Kustomize Docker image"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "shared-ci-cd" {
-  name         = "shared-ci-cd"
-  description  = "Shared CI/CD configurations"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "shared-ci-cd"
+  description      = "Shared CI/CD configurations"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "golang-template" {
-  name         = "golang-template"
-  description  = "Template for new Go applications"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "golang-template"
+  description      = "Template for new Go applications"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "golang-template-env" {
-  name         = "golang-template-env"
-  description  = "Template for new Go app environment repos"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "golang-template-env"
+  description      = "Template for new Go app environment repos"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "java-template" {
-  name         = "java-template"
-  description  = "Template for new Java applications"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "java-template"
+  description      = "Template for new Java applications"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_project" "java-template-env" {
-  name         = "java-template-env"
-  description  = "Template for new Java app environment repos"
-  namespace_id = "${gitlab_group.platform-admins.id}"
+  name             = "java-template-env"
+  description      = "Template for new Java app environment repos"
+  namespace_id     = gitlab_group.platform-admins.id
   visibility_level = "internal"
-  default_branch = "master"
+  default_branch   = "master"
 }
 
 resource "gitlab_deploy_key" "acm-staging-us-central1" {
-  project = "platform-admins/anthos-config-management"
-  title   = "Staging deploy key"
-  key     = "${file("${local.ssh-key-path}/staging-us-central1.pub")}"
-  depends_on = ["gitlab_project.anthos-config-management"]
+  project    = "platform-admins/anthos-config-management"
+  title      = "Staging deploy key"
+  key        = file("${local.ssh-key-path}/staging-us-central1.pub")
+  depends_on = [gitlab_project.anthos-config-management]
 }
 
 resource "gitlab_deploy_key" "acm-prod-us-central1" {
-  project = "platform-admins/anthos-config-management"
-  title   = "Production us-central1 deploy key"
-  key     = "${file("${local.ssh-key-path}/prod-us-central1.pub")}"
-  depends_on = ["gitlab_project.anthos-config-management"]
+  project    = "platform-admins/anthos-config-management"
+  title      = "Production us-central1 deploy key"
+  key        = file("${local.ssh-key-path}/prod-us-central1.pub")
+  depends_on = [gitlab_project.anthos-config-management]
 }
+
 resource "gitlab_deploy_key" "acm-prod-us-east1" {
-  project = "platform-admins/anthos-config-management"
-  title   = "Production East deploy key"
-  key     = "${file("${local.ssh-key-path}/prod-us-east1.pub")}"
-  depends_on = ["gitlab_project.anthos-config-management"]
+  project    = "platform-admins/anthos-config-management"
+  title      = "Production East deploy key"
+  key        = file("${local.ssh-key-path}/prod-us-east1.pub")
+  depends_on = [gitlab_project.anthos-config-management]
 }
 
 resource "gitlab_deploy_key" "local-user-kaniko-docker-push" {
-  project = "platform-admins/kaniko-docker"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/kaniko-docker.pub")}"
-  can_push = true
-  depends_on = ["gitlab_project.kaniko-docker"]
+  project    = "platform-admins/kaniko-docker"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/kaniko-docker.pub")
+  can_push   = true
+  depends_on = [gitlab_project.kaniko-docker]
 }
 
 resource "gitlab_deploy_key" "local-user-kustomize-docker-push" {
-  project = "platform-admins/kustomize-docker"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/kustomize-docker.pub")}"
-  can_push = true
-  depends_on = ["gitlab_project.kustomize-docker"]
+  project    = "platform-admins/kustomize-docker"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/kustomize-docker.pub")
+  can_push   = true
+  depends_on = [gitlab_project.kustomize-docker]
 }
 
 resource "gitlab_deploy_key" "local-user-acm-push" {
-  project = "platform-admins/anthos-config-management"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/anthos-config-management.pub")}"
-  can_push = true
-  depends_on = ["gitlab_project.anthos-config-management"]
+  project    = "platform-admins/anthos-config-management"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/anthos-config-management.pub")
+  can_push   = true
+  depends_on = [gitlab_project.anthos-config-management]
 }
 
 resource "gitlab_deploy_key" "local-user-kustomize-push" {
-  project = "platform-admins/shared-kustomize-bases"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/shared-kustomize-bases.pub")}"
-  depends_on = ["gitlab_project.shared-kustomize-bases"]
-  can_push = true
+  project    = "platform-admins/shared-kustomize-bases"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/shared-kustomize-bases.pub")
+  depends_on = [gitlab_project.shared-kustomize-bases]
+  can_push   = true
 }
 
 resource "gitlab_deploy_key" "local-user-ci-cd-push" {
-  project = "platform-admins/shared-ci-cd"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/shared-ci-cd.pub")}"
-  depends_on = ["gitlab_project.shared-ci-cd"]
-  can_push = true
+  project    = "platform-admins/shared-ci-cd"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/shared-ci-cd.pub")
+  depends_on = [gitlab_project.shared-ci-cd]
+  can_push   = true
 }
 
 resource "gitlab_deploy_key" "local-user-golang-template-push" {
-  project = "platform-admins/golang-template"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/golang-template.pub")}"
-  depends_on = ["gitlab_project.golang-template"]
-  can_push = true
+  project    = "platform-admins/golang-template"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/golang-template.pub")
+  depends_on = [gitlab_project.golang-template]
+  can_push   = true
 }
 
 resource "gitlab_deploy_key" "local-user-golang-template-env-push" {
-  project = "platform-admins/golang-template-env"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/golang-template-env.pub")}"
-  depends_on = ["gitlab_project.golang-template-env"]
-  can_push = true
+  project    = "platform-admins/golang-template-env"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/golang-template-env.pub")
+  depends_on = [gitlab_project.golang-template-env]
+  can_push   = true
 }
 
 resource "gitlab_deploy_key" "local-user-java-template-push" {
-  project = "platform-admins/java-template"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/java-template.pub")}"
-  depends_on = ["gitlab_project.java-template"]
-  can_push = true
+  project    = "platform-admins/java-template"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/java-template.pub")
+  depends_on = [gitlab_project.java-template]
+  can_push   = true
 }
 
 resource "gitlab_deploy_key" "local-user-java-template-env-push" {
-  project = "platform-admins/java-template-env"
-  title   = "Local User deploy key"
-  key     = "${file("${local.ssh-key-path}/java-template-env.pub")}"
-  depends_on = ["gitlab_project.java-template-env"]
-  can_push = true
+  project    = "platform-admins/java-template-env"
+  title      = "Local User deploy key"
+  key        = file("${local.ssh-key-path}/java-template-env.pub")
+  depends_on = [gitlab_project.java-template-env]
+  can_push   = true
 }
--- 2_gitlab/outputs.tf
+++ 2_gitlab/outputs.tf
@@ -14,13 +14,12 @@
  * limitations under the License.
  */
 
-
 output "gitlab_address" {
-  value       = "${data.google_compute_address.gitlab.address}"
+  value       = data.google_compute_address.gitlab.address
   description = "Point your wildcard domain to this IP address as an A record"
 }
 
 output "gitlab_domain" {
-  value       = "${var.domain}"
+  value       = var.domain
   description = "Domain used to deploy Gitlab."
 }
