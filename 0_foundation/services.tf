01378230444f945a290e52db97d239d24f010822
Morgante Pell <morgantep@google.com>
2020-05-14 21:05:45 -0400

    Upgrade foundation to use Terraform 0.12 syntax.

--- 0_foundation/services.tf
+++ 0_foundation/services.tf
@@ -15,38 +15,37 @@
  */
 
 resource "google_project_service" "compute" {
-  project = "${var.project_id}"
+  project = var.project_id
   service = "compute.googleapis.com"
 
   disable_dependent_services = true
-  depends_on = ["google_project_service.cloudresourcemanager"]
+  depends_on                 = [google_project_service.cloudresourcemanager]
 }
 
 resource "google_project_service" "container" {
-  project = "${var.project_id}"
+  project = var.project_id
   service = "container.googleapis.com"
 
   disable_dependent_services = true
 }
 
 resource "google_project_service" "cloudresourcemanager" {
-  project = "${var.project_id}"
+  project = var.project_id
   service = "cloudresourcemanager.googleapis.com"
 
   disable_dependent_services = true
 }
 
 resource "google_project_service" "sqladmin" {
-  project = "${var.project_id}"
+  project = var.project_id
   service = "sqladmin.googleapis.com"
 
   disable_dependent_services = true
 }
 
 resource "google_project_service" "artifactregistry" {
-  project = "${var.project_id}"
+  project = var.project_id
   service = "artifactregistry.googleapis.com"
 
   disable_dependent_services = true
 }
-
