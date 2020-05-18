79c0e50b47595a902318d7bc1f09810f235631e6
Mike! <ensor@google.com>
2020-05-18 13:45:37 -0700

    switching to module for api-enablement to be consistent with project

--- 1_clusters/binary-auth.tf
+++ 1_clusters/binary-auth.tf
@@ -30,14 +30,14 @@ locals {
 }
 
 # APIs required for Binary Authorization
-resource "google_project_service" "enabled-apis" {
-  for_each                   = toset(local.admin_enabled_apis)
-  project                    = var.project_id
-  service                    = each.value
-  disable_dependent_services = true
-  disable_on_destroy         = true
-}
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 8.0"
 
+  project_id = var.project_id
+
+  activate_apis = local.admin_enabled_apis
+}
 
 # Create Quality Assurance attestor
 module "quality-attestor" {
